e582885a27791a119a5dbc343dbecbb204012d5e
sauxpa <46052865+sauxpa@users.noreply.github.com>
2019-12-11 18:42:40 +0100

    more option for vietoris-rips

--- connectivity_representation_learning/models.py
+++ connectivity_representation_learning/models.py
@@ -7,23 +7,44 @@ from tqdm import tqdm
 
 from .utils import triangular_from_linear_index, linear_index_from_triangular
 
-def persistence_lengths(batch, dim=0, device=torch.device('cpu')):
+def persistence_lengths(batch, 
+                        dim=0, 
+                        device=torch.device('cpu'), 
+                        max_edge_length=np.inf,
+                       ):
     """Use Gudhi to calculate persistence diagrams.
 
     batch: point clouds input,
-    dim: homology dimension (0 for connectivity)
+    dim: homology dimension (0 for connectivity),
+    device: device for torch tensor,
+    max_edge_length: threshold on the Vietoris-Rips scale parameter. By default, it builds
+    all the simplices in the filtration.
     """
-    rips_complex = gd.RipsComplex(batch, max_edge_length=0.5)
-    simplex_tree = rips_complex.create_simplex_tree(max_dimension=2)
+    rips_complex = gd.RipsComplex(batch, max_edge_length=max_edge_length)
+    simplex_tree = rips_complex.create_simplex_tree(max_dimension=dim)
+#     simplex_tree = rips_complex.create_simplex_tree(max_dimension=2)
     simplex_tree.persistence(homology_coeff_field=2, min_persistence=0)
-    persistence_intervals = simplex_tree.persistence_intervals_in_dimension(dim)[:, 1]
-    return torch.FloatTensor(persistence_intervals).to(device)
+    persistence_intervals = simplex_tree.persistence_intervals_in_dimension(dim)
+    return torch.FloatTensor(persistence_intervals[:, 1]-persistence_intervals[:, 0]).to(device)
     
-def barcode_stats(data, dim=0, device=torch.device('cpu')):
+def barcode_stats(data, 
+                  dim=0, 
+                  device=torch.device('cpu'),
+                  max_edge_length=np.inf,
+                 ):
     """Min, average and max barcode lengths of data.
+    data: point clouds input,
+    dim: homology dimension (0 for connectivity),
+    device: device for torch tensor,
+    max_edge_length: threshold on the Vietoris-Rips scale parameter. By default, it builds
+    all the simplices in the filtration.
     """
     # remove inf
-    barcodes = np.ma.masked_invalid(persistence_lengths(data, dim=dim, device=device))
+    barcodes = np.ma.masked_invalid(persistence_lengths(data, 
+                                                        dim=dim,
+                                                        device=device,
+                                                        max_edge_length=max_edge_length,
+                                                       ))
     return [barcodes.min(), barcodes.mean(), barcodes.max()]
     
     
